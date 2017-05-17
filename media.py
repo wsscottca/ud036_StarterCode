f75c30c92ff5b46b78c1f06df19e937083c6030e
Will Scott <WSScottCA@gmail.com>
2017-05-17 12:48:19 -0700

    Add files via upload
    Added movie class and database for fresh tomatoes

---
+++ media.py
@@ -0,0 +1,10 @@
+import fresh_tomatoes
+
+# Define the Movie class template
+class Movie():
+    # Initialize instance variables
+    def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
+        self.title = movie_title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+        
